cd8ac971cb620fb6882aed15caeb7bbfb7df3f08
thomasyu888 <thomasyu888@gmail.com>
2020-03-12 23:38:35 -0700

    Add initial tests

--- tests/test_create.py
+++ tests/test_create.py
@@ -3,6 +3,7 @@ Tests creation module
 Functions are named with the function name in create module along
 with what is tested
 """
+import mock
 from mock import patch
 import uuid
 
@@ -15,9 +16,26 @@ SYN = mock.create_autospec(synapseclient.Synapse)
 
 def test_create_project__call():
     """Tests the correct parameters are passed in"""
-    project = str(uuid.uuid1())
-    returned = str(uuid.uuid2())
+    project_name = str(uuid.uuid1())
+    project = synapseclient.Project(name=project_name)
+    returned = synapseclient.Project(name=project_name,
+                                     id=str(uuid.uuid1()))
     with patch.object(SYN, "store", return_value=returned) as patch_syn_store:
-        project = create.create_project(SYN, project)
-        assert project == returned
+        new_project = create.create_project(SYN, project_name)
+        assert new_project == returned
         patch_syn_store.assert_called_once_with(project)
+
+
+def test_create_folder__call():
+    """Tests the correct parameters are passed in"""
+    folder_name = str(uuid.uuid1())
+    parentid = str(uuid.uuid1())
+    folder = synapseclient.Folder(name=folder_name,
+                                  parentId=parentid)
+    returned = synapseclient.Folder(name=folder_name,
+                                    id=str(uuid.uuid1()),
+                                    parentId=parentid)
+    with patch.object(SYN, "store", return_value=returned) as patch_syn_store:
+        new_folder = create.create_folder(SYN, folder_name, parentid)
+        assert new_folder == returned
+        patch_syn_store.assert_called_once_with(folder)
