9664e11f993893ea37d9209a5ca07918c6a6e8bd
thomasyu888 <thomas.yu@sagebase.org>
2021-04-15 13:50:19 +0800

    Add in code to expand shorten hand configuration to 'official' tempalte

--- synapseformation/client.py
+++ synapseformation/client.py
@@ -20,6 +20,39 @@ def get_yaml_config(template_path: str) -> dict:
     return template
 
 
+def expand_config(config):
+    """Expands shortened configuration to the official json format"""
+    # TODO: Once all resources are either under a key mapping or lists
+    # this will have to chance
+    if isinstance(config, dict) and config.get('type') == "Project":
+        # TODO: figure out how to clean this up.
+        # Remove yaml anchor objects as they are expanded
+        to_remove = [key for key in config
+                     if key not in ["type", "name", "children"]]
+        for key in to_remove:
+            config.pop(key)
+        # Get children if exists
+        children = config.get('children')
+        if children is not None:
+            expand_config(children)
+    else:
+        # Loop through folders and create them
+        for index, folder in enumerate(config):
+            # The folder configuration can be lists or dicts
+            # Must pull out children if it exists
+            if isinstance(folder, dict):
+                children = folder.get('children')
+            else:
+                config[index] = {"name": folder,
+                                 "type": "Folder"}
+                children = None
+
+            # Create nested folders
+            if children is not None:
+                expand_config(children)
+    return config
+
+
 def _create_synapse_resources_yamlanchor(syn: Synapse, config: dict,
                                          parentid: str = None):
     """Creates synapse resources using yaml anchor template
@@ -64,4 +97,5 @@ def create_synapse_resources(template_path: str):
     """Creates synapse resources from template"""
     syn = synapseclient.login()
     config = get_yaml_config(template_path)
+    full_config = expand_config(config)
     _create_synapse_resources_yamlanchor(syn, config)
