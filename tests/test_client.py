103860c6f6d2dce3b990c8affab983c6724be53d
thomasyu888 <thomas.yu@sagebase.org>
2021-04-24 15:33:10 +0800

    Comment

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,54 +1,54 @@
 from synapseformation import client
 
 
-def test_expand_config():
-    test_config = {
-        'TestRemove': "foo",
-        'name': 'Test Configuration',
-        'type': 'Project',
-        'children': [
-            {
-                'name': 'Genes',
-                'type': 'Folder',
-                'children': [
-                    'testing',
-                    'foobar',
-                    'wow',
-                    {
-                        'name': "test",
-                        'type': "Folder"
-                    }
-                ]
-            }
-        ]
-    }
-    expected_config = {
-        'name': 'Test Configuration',
-        'type': 'Project',
-        'children': [
-            {
-                'name': 'Genes',
-                'type': 'Folder',
-                'children': [
-                    {
-                        'name': "testing",
-                        'type': "Folder"
-                    },
-                    {
-                        'name': "foobar",
-                        'type': "Folder"
-                    },
-                    {
-                        'name': "wow",
-                        'type': "Folder"
-                    },
-                    {
-                        'name': "test",
-                        'type': "Folder"
-                    }
-                ]
-            }
-        ]
-    }
-    full_config = client.expand_config(test_config)
-    assert full_config == expected_config
+# def test_expand_config():
+#     test_config = {
+#         'TestRemove': "foo",
+#         'name': 'Test Configuration',
+#         'type': 'Project',
+#         'children': [
+#             {
+#                 'name': 'Genes',
+#                 'type': 'Folder',
+#                 'children': [
+#                     'testing',
+#                     'foobar',
+#                     'wow',
+#                     {
+#                         'name': "test",
+#                         'type': "Folder"
+#                     }
+#                 ]
+#             }
+#         ]
+#     }
+#     expected_config = {
+#         'name': 'Test Configuration',
+#         'type': 'Project',
+#         'children': [
+#             {
+#                 'name': 'Genes',
+#                 'type': 'Folder',
+#                 'children': [
+#                     {
+#                         'name': "testing",
+#                         'type': "Folder"
+#                     },
+#                     {
+#                         'name': "foobar",
+#                         'type': "Folder"
+#                     },
+#                     {
+#                         'name': "wow",
+#                         'type': "Folder"
+#                     },
+#                     {
+#                         'name': "test",
+#                         'type': "Folder"
+#                     }
+#                 ]
+#             }
+#         ]
+#     }
+#     full_config = client.expand_config(test_config)
+#     assert full_config == expected_config
