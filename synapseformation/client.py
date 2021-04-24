b74399d56d7a97d31998e324ee35cf439f87b8e5
thomasyu888 <thomas.yu@sagebase.org>
2021-04-24 15:32:11 +0800

    comment out expansion code for now

--- synapseformation/client.py
+++ synapseformation/client.py
@@ -6,37 +6,37 @@ from .create import SynapseCreation
 from . import utils
 
 
-def expand_config(config: dict) -> dict:
-    """Expands shortened configuration to the official json format"""
-    # TODO: Once all resources are either under a key mapping or lists
-    # this will have to chance
-    if isinstance(config, dict) and config.get('type') == "Project":
-        # TODO: figure out how to clean this up.
-        # Remove yaml anchor objects as they are expanded
-        to_remove = [key for key in config
-                     if key not in ["type", "name", "children"]]
-        for key in to_remove:
-            config.pop(key)
-        # Get children if exists
-        children = config.get('children')
-        if children is not None:
-            expand_config(children)
-    else:
-        # Loop through folders and create them
-        for index, folder in enumerate(config):
-            # The folder configuration can be lists or dicts
-            # Must pull out children if it exists
-            if isinstance(folder, dict):
-                children = folder.get('children')
-            else:
-                config[index] = {"name": folder,
-                                 "type": "Folder"}
-                children = None
+# def expand_config(config: dict) -> dict:
+#     """Expands shortened configuration to the official json format"""
+#     # TODO: Once all resources are either under a key mapping or lists
+#     # this will have to chance
+#     if isinstance(config, dict) and config.get('type') == "Project":
+#         # TODO: figure out how to clean this up.
+#         # Remove yaml anchor objects as they are expanded
+#         to_remove = [key for key in config
+#                      if key not in ["type", "name", "children"]]
+#         for key in to_remove:
+#             config.pop(key)
+#         # Get children if exists
+#         children = config.get('children')
+#         if children is not None:
+#             expand_config(children)
+#     else:
+#         # Loop through folders and create them
+#         for index, folder in enumerate(config):
+#             # The folder configuration can be lists or dicts
+#             # Must pull out children if it exists
+#             if isinstance(folder, dict):
+#                 children = folder.get('children')
+#             else:
+#                 config[index] = {"name": folder,
+#                                  "type": "Folder"}
+#                 children = None
 
-            # Create nested folders
-            if children is not None:
-                expand_config(children)
-    return config
+#             # Create nested folders
+#             if children is not None:
+#                 expand_config(children)
+#     return config
 
 
 def _create_synapse_resources(syn: Synapse, config: dict,
@@ -50,10 +50,9 @@ def _create_synapse_resources(syn: Synapse, config: dict,
     """
     creation_cls = SynapseCreation(syn)
     if isinstance(config, dict) and config.get('type') == "Project":
-        # TODO: remove this testing code
-        config['name'] = "tyu-test TREAD_AD_YAML"
         project = creation_cls.get_or_create_project(name=config['name'])
         parent_id = project.id
+        config['id'] = parent_id
         # Get children if exists
         children = config.get('children')
         if children is not None:
@@ -67,6 +66,7 @@ def _create_synapse_resources(syn: Synapse, config: dict,
             folder_ent = creation_cls.get_or_create_folder(
                 name=folder_name, parentId=parentid
             )
+            folder['id'] = folder_ent.id
             # Create nested folders
             if children is not None:
                 _create_synapse_resources(syn, children, folder_ent.id)
@@ -79,6 +79,9 @@ def create_synapse_resources(template_path: str):
     config = utils.read_config(template_path)
     # Expands shortended configuration into full configuration.  This should
     # work if full configuration is passed in
-    full_config = expand_config(config)
+    # TODO: Ignore expansion of configuration for now
+    # full_config = expand_config(config)
     # Recursive function to create resources
-    _create_synapse_resources(syn, full_config)
+    for resource in config:
+        _create_synapse_resources(syn, resource)
+    print(config)
