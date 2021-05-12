ee47e7769663c7ac948da7bf04a3b255d68813c1
thomasyu888 <thomas.yu@sagebase.org>
2021-05-12 23:46:40 +0800

    Add test

--- tests/test_client.py
+++ tests/test_client.py
@@ -64,6 +64,7 @@ from synapseformation.create import SynapseCreation
 class TestCreateSynapseResources():
 
     def setup_method(self):
+        """Setting up for each method"""
         self.syn = mock.create_autospec(synapseclient.Synapse)
         self.create_cls = SynapseCreation(self.syn)
         self.config = {
@@ -101,3 +102,40 @@ class TestCreateSynapseResources():
                                              creation_cls=self.create_cls)
             patch_create.assert_called_once_with(name=project_config['name'])
             assert project_config == expected_config
+
+    def test__create_synapse_resources_folder(self):
+        """Test folders gets created"""
+        folder_config = [
+            {
+                'name': 'Test 1',
+                'type': 'Folder'
+            },
+            {
+                'name': 'Test 2',
+                'type': 'Folder'
+            }
+        ]
+        expected_config = [
+            {
+                'name': 'Test 1',
+                'type': 'Folder',
+                'id': 'syn33333'
+            },
+            {
+                'name': 'Test 2',
+                'type': 'Folder',
+                'id': 'syn22222'
+            }
+        ]
+        folder_ent_1 = synapseclient.Folder(id="syn33333", parentId="syn5555")
+        folder_ent_2 = synapseclient.Folder(id="syn22222", parentId="syn5555")
+        call_1 = mock.call(name="Test 1", parentId="syn5555")
+        call_2 = mock.call(name="Test 2", parentId="syn5555")
+        with patch.object(self.create_cls, "get_or_create_folder",
+                          side_effect=[folder_ent_1,
+                                       folder_ent_2]) as patch_create:
+            client._create_synapse_resources(config=folder_config,
+                                             creation_cls=self.create_cls,
+                                             parentid="syn5555")
+            patch_create.assert_has_calls([call_1, call_2])
+            assert folder_config == expected_config
